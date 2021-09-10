20cffd8a42dc5241e37c1fd4e5b1916206f469d0
Ihor <ihor.fesyk@nixsolutions.com>
2021-09-10 18:13:29 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import SignUp from "./pages/signup/signup"
+import InboxIcon from "./assets/inboxIcon"
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" flex justify-center items-center bg-gray-50 w-screen h-screen">
+      <div className="flex w-11/12 h-5/6  shadow-md">
+        {/* <SignUp /> */}
+        <div className="w-1/5 bg-white p-5 border-r-2 border-gray-50 rounded-l-lg">
+          Sidebar
+          <div className="flex mt-5 text-gray-700 rounded cursor-pointer hover:bg-gray-100 p-2">
+            <InboxIcon />
+            <span className="ml-1">Inbox</span>
+          </div>
+        </div>
+        <div className="w-4/5 bg-white p-5 rounded-r-lg">Main Content</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
